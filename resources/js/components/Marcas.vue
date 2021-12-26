504f07f6832d3d7634545d88d42c5e5628df1a09
Antonio Pires <tonan@globo.com>
2021-12-26 16:58:34 -0300

    348. Criando um componente Vue para encapsular inputs

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="container">
+  <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
@@ -9,14 +9,14 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col mb-3">
-                                <label for="inputId" class="form-label">ID</label>
-                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                                <div id="idHelp" class="form-text text-muted">Opcional. Informe o ID do Registro</div>
+                                <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da Marca">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                </input-container-component>                          
                             </div>
                             <div class="col mb-3">
-                                <label for="inputNome" class="form-label">Nome</label>
-                                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da Marca">
-                                <div id="nomeHelp" class="form-text text-muted">Opcional. Informe o Nome da Marca</div>
+                                <input-container-component titulo="Nome da Marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o Nome da Marca">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da Marca">
+                                </input-container-component>
                             </div>
                         </div>
                     </div>
@@ -32,5 +32,6 @@
 </template>
 
 <script>
+
 
 </script>
