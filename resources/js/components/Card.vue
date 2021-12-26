505ee10b3a250bf9d86f9aefb785b57429bba525
Antonio Pires <tonan@globo.com>
2021-12-26 17:26:25 -0300

    351. Criando um componente Vue para cards

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="card mb-3">
+    <!-- Início do Card de Busca -->
+    <div class="card-header">{{titulo}}</div>
+
+    <div class="card-body">
+        <div class="row">
+            <slot name="conteudo"></slot>
+        </div>
+    </div>
+
+    <div class="card-footer">
+        <slot name="rodape"></slot>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['titulo']
+    }
+</script>
--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -3,45 +3,40 @@
   <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card mb-3">
-                    <!-- Início do Card de Busca -->
-                    <div class="card-header">Busca de Marcas</div>
-
-                    <div class="card-body">
+                <!-- Início do Card de Busca -->
+                <card-component titulo="Busca de Marcas">
+                    <template v-slot:conteudo>
                         <div class="row">
-                            <div class="col mb-3">
-                                <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da Marca">
-                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                                </input-container-component>                          
-                            </div>
-                            <div class="col mb-3">
-                                <input-container-component titulo="Nome da Marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o Nome da Marca">
-                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da Marca">
-                                </input-container-component>
-                            </div>
+                        <div class="col mb-3">
+                            <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da Marca">
+                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                            </input-container-component>                          
+                        </div>
+                        <div class="col mb-3">
+                            <input-container-component titulo="Nome da Marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o Nome da Marca">
+                                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da Marca">
+                            </input-container-component>
                         </div>
                     </div>
+                    </template>
 
-                    <div class="card-footer">
+                    <template v-slot:rodape>
                         <button type="submit" class="btn btn-primary btn-sm" style="float: right;">Pesquisar</button>
-                    </div>
-                </div>
+                    </template>
+                </card-component>
                 <!-- Fim do card de busca -->
 
                 <!-- Inicio do card de listagem de marcas -->
-                <div class="card">
-                    <div class="card-header">Relação de Marcas</div>
+                <card-component titulo="Relação de Marcas">
+                    <template v-slot:conteudo>
                         <table-component></table-component>
-                    <div class="card-body">
-                        
-                    </div>
+                    </template>
 
-                    <div class="card-footer">
+                    <template v-slot:rodape>
                         <button type="button" class="btn btn-primary btn-sm" style="float: right;">Adicionar</button>
-                    </div>
-                </div>
+                    </template>
+                </card-component>
                 <!-- Fim do Card de listagem de marcas -->
-
             </div>
         </div>
     </div>
