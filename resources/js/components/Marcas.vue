d101288a4e710a1780a8e78e0137e7cb29cef1da
Antonio Pires <tonan@globo.com>
2021-12-29 06:42:55 -0300

    367. Adicionando a paginação de registros parte 1

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -30,7 +30,7 @@
                 <card-component titulo="Relação de Marcas">
                     <template v-slot:conteudo>
                         <table-component 
-                        :dados="marcas"
+                        :dados="marcas.data"
                         :titulos="{
                             id: {titulo: 'ID', tipo: 'texto'},
                             nome: {titulo: 'Nome', tipo: 'texto'},
